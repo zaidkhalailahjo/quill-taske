b0a455a714b47cf350305049d767bf1288a96d92
zaidkhalailahjo <zaidkhalailahjo@gmail.com>
2026-04-29 12:53:15 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,4 +1,3 @@
-// js/firebase-config.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-firestore.js";
@@ -13,21 +12,7 @@ const firebaseConfig = {
     measurementId: "G-BMEZ49L6CN"
 };
 
-// تهيئة المشروع الأساسي
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 export const appId = 'quill-world-system';
-
-// تهيئة مشروع التخزين المنفصل (إن وجد)
-const firebaseConfigStorage = {
-    apiKey: "AIzaSyBjPerebbHAtkKWJV_LFkoaL-15oTNHRdg",
-    authDomain: "quill-worl.firebaseapp.com",
-    projectId: "quill-worl",
-    storageBucket: "quill-worl.firebasestorage.app",
-    messagingSenderId: "192227363160",
-    appId: "1:192227363160:web:59a620429b4bfdfb7cfd2d",
-    measurementId: "G-NLY5LN0830"
-};
-export const appStorage = initializeApp(firebaseConfigStorage, "StorageApp");
-export const dbStorage = getFirestore(appStorage);
